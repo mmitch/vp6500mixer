4f95ecca6773fe2c9126c1a32ba107d4d8c074f0
Christian Garbs <mitch@cgarbs.de>
2010-05-24 14:19:48 +0200

    add license information

--- vp6500mixer.c
+++ vp6500mixer.c
@@ -1,3 +1,24 @@
+/*
+ * vp6500mixer - sound mixer utility für Philips VP6500 VoIP phone
+ *
+ * Copyright (C) 2010  Christian Garbs <mitch@cgarbs.de>
+ * licensed under GNU GPL v2 or later
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <fcntl.h>
 #include <stdio.h>
 #include <sys/ioctl.h>
